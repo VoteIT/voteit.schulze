d95143f47bb624bbfb983721d7f5a905f2afab97
Fredrik Nilson <fredrik@betahaus.net>
2012-03-07 16:15:50 +0100

    pinning colander to 0.9.5 because of problems with translations of default values

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
 requires = (
     'pyramid',
-    'colander',
+    'colander==0.9.5',
     'deform',
     'voteit.core',
     'python-vote-core',
