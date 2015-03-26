b0aaa1bf297df9a746ee81621865978deee40f33
robinharms <robin@betahaus.net>
2015-03-26 18:14:52 +0100

    Always include resources within a meeting context since voting forms can be opened anywhere.

--- voteit/schulze/fanstatic_lib.py
+++ voteit/schulze/fanstatic_lib.py
@@ -1,9 +1,9 @@
 from arche.interfaces import IViewInitializedEvent
+from arche.interfaces import IBaseView
 from fanstatic import Library
 from fanstatic import Resource
 from fanstatic import Group
 from js.jquery import jquery
-from voteit.core.views.agenda_item import AgendaItemView
 
 
 library = Library('voteit_schulze', 'static')
@@ -15,13 +15,13 @@ star_rating = Group((_star_rating_css, _star_rating_js))
 
 #The old jquery rating cruft is in deed need of an overhaul.
 
-def need_star_rating(*args):
-    star_rating.need()
+def need_star_rating(view, event):
+    """ Always load star rating within meetings
+        since poll forms can be opened anywhere now.
+    """
+    if view.request.meeting:
+        star_rating.need()
 
 def includeme(config):
-    """ Create a subscriber that fires on agenda items. This isn't a perfect way
-        of including this script, but there's no other good way right now.
-        FIXME: Change this :)
-    """
-    config.add_subscriber(need_star_rating, [AgendaItemView, IViewInitializedEvent])
+    config.add_subscriber(need_star_rating, [IBaseView, IViewInitializedEvent])
      
