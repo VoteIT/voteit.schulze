c319d1db7648a5c77a300e33a52305b803488d6a
robinharms <robin@betahaus.net>
2015-04-14 10:04:12 +0200

    New implementation of rating widget

--- voteit/schulze/fanstatic_lib.py
+++ voteit/schulze/fanstatic_lib.py
@@ -2,18 +2,11 @@ from arche.interfaces import IViewInitializedEvent
 from arche.interfaces import IBaseView
 from fanstatic import Library
 from fanstatic import Resource
-from fanstatic import Group
-from js.jquery import jquery
 
 
 library = Library('voteit_schulze', 'static')
+star_rating = Resource(library, 'rating.css')
 
-_star_rating_css = Resource(library, 'rating.css')
-_star_rating_js = Resource(library, 'jquery.rating.js', depends = (jquery,))
-
-star_rating = Group((_star_rating_css, _star_rating_js))
-
-#The old jquery rating cruft is in deed need of an overhaul.
 
 def need_star_rating(view, event):
     """ Always load star rating within meetings
