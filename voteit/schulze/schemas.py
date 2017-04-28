dc15ecc14c259192b55d30336b66adb5e4ebd214
Robin Harms Oredsson <robin@betahaus.net>
2017-04-28 14:53:14 +0200

    Text-fixes

--- voteit/schulze/schemas.py
+++ voteit/schulze/schemas.py
@@ -7,24 +7,32 @@ from voteit.schulze import _
 class SettingsSchema(colander.Schema):
     """ Settings for a Schulze poll
     """
-    winners = colander.SchemaNode(colander.Int(),
-                                  title = _(u"Winners"),
-                                  description = _(u"schulze_config_winners_description",
-                                                  default=u"Numbers of possible winners in the poll"),
-                                  default=1,)
-    max_stars = colander.SchemaNode(colander.Int(),
-                                    title = _(u"Maximum stars"),
-                                    description = _(u"schulze_config_max_stars_description",
-                                                    default=u"The maximum numbers of stars regarless of number of proposals"),
-                                    default=5,)
-    min_stars = colander.SchemaNode(colander.Int(),
-                                    title = _(u"Minumum stars"),
-                                    description = _(u"schulze_config_min_stars_description",
-                                                    default=u"The minimum numbers of stars regarless of number of proposals"),
-                                    default=5,)
+    winners = colander.SchemaNode(
+        colander.Int(),
+        title=_(u"Winners (For 1, use regular Schulze)"),
+        description=_(u"schulze_config_winners_description",
+                      default="Note! With more than 5 winners, "
+                              "the result may take an extremely long time to calculate. "
+                              "Consult VoteIT development team before using this!"),
+        default=2,
+    )
+    max_stars = colander.SchemaNode(
+        colander.Int(),
+        title=_(u"Maximum stars"),
+        description=_(u"schulze_config_max_stars_description",
+                      default=u"The maximum numbers of stars regarless of number of proposals"),
+        default=5,
+    )
+    min_stars = colander.SchemaNode(
+        colander.Int(),
+        title=_(u"Minumum stars"),
+        description=_(u"schulze_config_min_stars_description",
+                      default=u"The minimum numbers of stars regarless of number of proposals"),
+        default=5,
+    )
 
 
-#The schema will be populated in the schulze poll plugin
+# The schema will be populated in the schulze poll plugin
 class SchulzePollSchema(colander.Schema):
-    widget = deform.widget.FormWidget(template = 'form_modal',
-                                      readonly_template = 'readonly/form_modal')
+    widget = deform.widget.FormWidget(template='form_modal',
+                                      readonly_template='readonly/form_modal')
